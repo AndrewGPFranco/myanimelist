aad712e32900a6eee7f47f04095852a1c7cf3527
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-03-20 23:18:50 -0300

    fazendo alguns testes com o prisma orm e entendendo melhor o ecosistema do Nuxt

---
+++ pages/users/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <p v-if="!users.length">Nenhum usuário cadastrado</p>
+    <ul v-else>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+    <router-link to="/">Voltar</router-link>
+  </div>
+</template>
+
+<script setup>
+const {data: users, error} = useFetch("/api/users/user");
+
+if (error.value)
+  console.error("Erro ao buscar usuários:", error.value)
+</script>
